81d4453b015cb47be296b6c5875a6fef755c7280
yusufpras23 <prasetiayusuf700@gmail.com>
2024-11-26 21:29:34 +0700

    nama

--- src/app/(public)/page.js
+++ src/app/(public)/page.js
@@ -32,7 +32,7 @@ export default function Dashboard() {
         <div className='flex w-full bg-white rounded-xl '>
           <div className="flex-1 p-10">
             <div className="font-extrabold text-[48px] mt-10">
-              <div>I&lsquo;m Rayan Adlrdard</div>
+              <div>I&lsquo; Yusuf Prastia</div>
               <div>
                 <span className="text-[#FFB400]">Front-end</span> Developer 
               </div>
